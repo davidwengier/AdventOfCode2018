f84c04511ce5b8128f13da31e38badf5a33c0b21
David Wengier <git@wengier.com>
2018-12-07 21:49:57 +1100

    Day 7 took it up a notch!

--- Day7.cs
+++ Day7.cs
@@ -10,71 +10,123 @@ namespace AdventOfCode
     {
         internal static void Part1()
         {
+            var nodes = GetNodes();
+
+            var toProcess = new SortedSet<RuleNode>();
+            var done = new HashSet<char>();
+            foreach (var n in nodes.Values.Where(n => n.Previous.Count == 0))
+            {
+                toProcess.Add(n);
+            }
+            int i = 0;
+            while (toProcess.Count > 0)
+            {
+                RuleNode next = toProcess.ElementAt(i);
+                if (next.Previous.All(n => done.Contains(n.Name)))
+                {
+                    done.Add(next.Name);
+                    toProcess.Remove(next);
+                    i = 0;
+                }
+                else
+                {
+                    i++;
+                    toProcess.Add(next);
+                }
+                foreach (var n in next.Next.OrderBy(n => n.Name))
+                {
+                    toProcess.Add(n);
+                }
+            }
+
+            Console.WriteLine(string.Join("", done));
+        }
+
+        private static Dictionary<char, RuleNode> GetNodes()
+        {
             var nodes = new Dictionary<char, RuleNode>();
-            var rules = new List<Rule>();
             foreach (string rule in _input)
             {
                 var r = new Rule(rule);
-                nodes[r.StepName] = new RuleNode(r.StepName);
-                nodes[r.PreceedingStepName] = new RuleNode(r.PreceedingStepName);
-                rules.Add(r);
-            }
-
-            foreach (var rule in rules)
-            {
-                var node = nodes[rule.StepName];
-                foreach (var r in rules)
+                if (!nodes.ContainsKey(r.StepName))
                 {
-                    if (r.StepName == rule.StepName && rule.PreceedingStepName > 0)
-                    {
-                        var otherNode = nodes[r.PreceedingStepName];
-                        node.Previous.Add(otherNode);
-                        otherNode.Next.Add(node);
-
-                    }
+                    nodes.Add(r.StepName, new RuleNode(r.StepName));
                 }
-                foreach (var r in rules)
+                if (!nodes.ContainsKey(r.PreceedingStepName))
                 {
-                    if (r.PreceedingStepName == rule.StepName)
-                    {
-                        var otherNode = nodes[r.StepName];
-                        node.Next.Add(otherNode);
-                        otherNode.Previous.Add(node);
-                    }
+                    nodes.Add(r.PreceedingStepName, new RuleNode(r.PreceedingStepName));
                 }
+                nodes[r.StepName].Previous.Add(nodes[r.PreceedingStepName]);
+                nodes[r.PreceedingStepName].Next.Add(nodes[r.StepName]);
             }
 
-            var result = new HashSet<char>();
-            GetNode(nodes, r => r.Previous.Count == 0, result);
-            
-            Console.WriteLine(string.Join("", result));
-
-        }
-
-        private static void GetNode(Dictionary<char, RuleNode> nodes, Func<RuleNode, bool> p, HashSet<char> storySoFar)
-        {
-            foreach (var node in nodes.Values.Where(p).OrderBy(r => r.Name))
-            {
-                storySoFar.Add(node.Name);
-                GetNextNodes(node, storySoFar);
-            }
-        }
-
-        private static void GetNextNodes(RuleNode node, HashSet<char> storySoFar)
-        {
-            foreach (var n in node.Next.OrderBy(n => n.Name))
-            {
-                if (n.Previous.All(n2 => storySoFar.Contains(n2.Name)))
-                {
-                    storySoFar.Add(n.Name);
-                    GetNextNodes(n, storySoFar);
-                }
-            }
+            return nodes;
         }
 
         internal static void Part2()
         {
+            var nodes = GetNodes();
 
+            int numWorkers = 5;
+            int extraTime = 60;
+
+            int secs = 0;
+            var workers = new WorkerInfo[numWorkers];
+            for (int j = 0; j < numWorkers; j++)
+            {
+                workers[j] = new WorkerInfo();
+            }
+
+
+            var toProcess = new SortedSet<RuleNode>();
+            var done = new HashSet<char>();
+            foreach (var n in nodes.Values.Where(n => n.Previous.Count == 0))
+            {
+                toProcess.Add(n);
+            }
+            while (toProcess.Count > 0 || done.Count != nodes.Count)
+            {
+                // see if any have finished
+                foreach (var worker in workers)
+                {
+                    if (worker.Working)
+                    {
+                        if (worker.FinishTime == secs)
+                        {
+                            done.Add(worker.Job);
+                            worker.Working = false;
+                        }
+                    }
+                }
+
+
+                RuleNode next = toProcess.FirstOrDefault(n => n.Previous.All(n2 => done.Contains(n2.Name)));
+                while (next != null && workers.Any(w => !w.Working))
+                {
+                    // find a worker to do this
+                    foreach (var worker in workers)
+                    {
+                        if (!worker.Working)
+                        {
+                            worker.Working = true;
+                            worker.Job = next.Name;
+                            worker.FinishTime = secs + extraTime + (worker.Job - 'A' + 1);
+                            break;
+                        }
+                    }
+                    toProcess.Remove(next);
+                    foreach (var n in next.Next.OrderBy(n => n.Name))
+                    {
+                        toProcess.Add(n);
+                    }
+                    next = toProcess.FirstOrDefault(n => n.Previous.All(n2 => done.Contains(n2.Name)));
+                }
+
+                secs++;
+            }
+
+            Console.WriteLine(string.Join("", done));
+            Console.WriteLine(secs-1);
         }
 
         [DebuggerDisplay("{PreceedingStepName} -> {StepName}")]
@@ -94,7 +146,7 @@ namespace AdventOfCode
         }
 
         [DebuggerDisplay("{Name}")]
-        private class RuleNode
+        private class RuleNode : IComparable<RuleNode>
         {
             public RuleNode(char name)
             {
@@ -103,6 +155,18 @@ namespace AdventOfCode
             public char Name { get; }
             public HashSet<RuleNode> Previous { get; } = new HashSet<RuleNode>();
             public HashSet<RuleNode> Next { get; } = new HashSet<RuleNode>();
+
+            int IComparable<RuleNode>.CompareTo(RuleNode other)
+            {
+                return Name.CompareTo(other.Name);
+            }
+        }
+
+        private class WorkerInfo
+        {
+            public bool Working { get; internal set; }
+            public int FinishTime { get; internal set; }
+            public char Job { get; internal set; }
         }
     }
 }
